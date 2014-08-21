fb1d6af38d702d05dc2ff5a6f503d3aed0e29815
Maurizio Mangione <granzebru@gmail.com>
2014-08-21 23:46:00 +0200

    watchify

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,58 @@
 'use strict';
 
 var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    changed = require('gulp-changed'),
     sass = require('gulp-sass'),
+    csso = require('gulp-csso'),
     autoprefixer = require('gulp-autoprefixer'),
     browserify = require('browserify'),
-    transform = require('vinyl-transform'),
+    watchify = require('watchify'),
+    source = require('vinyl-source-stream'),
     reactify = require('reactify'),
-    uglify = require('gulp-uglify'),
-    clean = require('gulp-clean'),
-    rename = require('gulp-rename'),
+    uglifyify = require('uglifyify'),
+    del = require('del'),
     webserver = require('gulp-webserver');
 
-gulp.task('clean', function() {
-  return gulp.src('dist', {read: false})
-    .pipe(clean());
+gulp.task('clean', function (cb) {
+  del(['dist'], cb);
 });
 
 gulp.task('webserver', function() {
   gulp.src('./')
     .pipe(webserver({
       livereload: true,
-      directoryListing: true,
       open: true
     }));
 });
 
-gulp.task('scripts', function () {
-  var browserified = transform(function (filename) {
-    var b = browserify(filename);
-    b.transform(reactify);
-    return b.bundle();
-  });
-  return gulp.src('scripts/app.jsx')
-    .pipe(browserified)
-    .pipe(uglify())
-    .pipe(rename('app.js'))
-    .pipe(gulp.dest('dist/js'));
+gulp.task('watchify', function () {
+  var bundler = watchify(browserify('./scripts/app.jsx', watchify.args));
+
+  function rebundle () {
+    return bundler.bundle()
+      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+      .pipe(source('app.js'))
+      .pipe(gulp.dest('dist/js'));
+  }
+
+  bundler.transform(reactify);
+  bundler.transform(uglifyify);
+  bundler.on('update', rebundle);
+  return rebundle();
 });
 
 gulp.task('styles', function () {
   return gulp.src('styles/main.scss')
+    .pipe(changed('dist/css'))
     .pipe(sass({sourceComments: 'map', sourceMap: 'sass'}))
     .pipe(autoprefixer('last 1 version'))
+    .pipe(csso())
     .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('scripts/**/*.jsx', ['scripts']);
   gulp.watch('styles/main.scss', ['styles']);
 });
 
-gulp.task('default', ['webserver', 'watch', 'scripts', 'styles']);
+gulp.task('default', ['webserver', 'watch', 'watchify', 'styles']);
