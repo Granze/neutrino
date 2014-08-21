a3da6f16d6a82c7ffde94602dab87a9ca3d5bd18
Maurizio Mangione <granzebru@gmail.com>
2014-08-21 19:50:48 +0200

    output sanitization

--- scripts/components/preview-area.jsx
+++ scripts/components/preview-area.jsx
@@ -3,19 +3,23 @@
 'use strict';
 
 var React = require('react'),
-    marked = require('ultramarked'),
+    marked = require('ultramarked');
 
-    PreviewArea = React.createClass({
-      render: function() {
-        return (
-          <div
-            className="PreviewArea"
-            dangerouslySetInnerHTML={{
-              __html: marked(this.props.content)
-            }}
-          />
-        );
-      }
+    marked.setOptions({
+      ultrasanitize: true
     });
+
+var PreviewArea = React.createClass({
+  render: function () {
+    return (
+      <div
+        className="PreviewArea"
+        dangerouslySetInnerHTML={{
+          __html: marked(this.props.content)
+        }}
+      />
+    );
+  }
+});
 
 module.exports =  PreviewArea;
