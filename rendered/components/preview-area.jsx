64085040af640496170def65ad0756a4f858cbf6
Maurizio Mangione <granzebru@gmail.com>
2015-06-04 00:03:34 +0200

    removed unused option

--- rendered/components/preview-area.jsx
+++ rendered/components/preview-area.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import marked from 'marked';
 
-marked.setOptions({
-  ultrasanitize: true
-});
-
 let PreviewArea = React.createClass({
   render: function () {
     return (
