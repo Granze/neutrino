4ba799dc8a4704b76c5fc62d316dd44617687829
Mauro Verrocchio <mauroverrocchio@gmail.com>
2015-06-03 22:58:06 +0200

    fixes ace-editor component update

--- rendered/components/input-area.jsx
+++ rendered/components/input-area.jsx
@@ -7,25 +7,19 @@ let editor;
 
 let InputArea = React.createClass({
 
-  handleChange: function() {
-    console.log('change');
-    this.props.onUserInput(
-      editor.setValue(this.props.content)
-    );
-  },
   componentDidMount: function() {
     editor = ace.edit('input');
     editor.getSession().setMode('ace/mode/markdown');
     editor.setTheme('ace/theme/github');
     editor.setFontSize(22);
     editor.setValue(this.props.content);
-  },
-  componentDidUpdate: function() {
-    editor.setValue(this.props.content);
+    editor.on('change', function() {
+      this.props.onUserInput(editor.getValue());
+    }.bind(this));
   },
   render: function() {
     return (
-      <div onChange={this.handleChange} id="input" className="input-area"></div>
+      <div id="input" className="input-area"></div>
     );
   }
 
